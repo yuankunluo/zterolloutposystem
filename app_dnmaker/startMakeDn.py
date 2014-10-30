f2f3b9636598fff90d0061a7d239b66a876bd28f
yuankunluo <yuankun1988@gmail.com>
2014-10-30 14:29:38 +0100

    spliting steps as functions in starMakeDn

--- app_dnmaker/startMakeDn.py
+++ app_dnmaker/startMakeDn.py
@@ -66,44 +66,6 @@ def checkZTEPowithSAPLieferRecordAndOutput(fileDict):
     sap_DNRecords = fileDict["sap_DNRecords"]
     orbm_Records = fileDict["orbm_Records"]
 
-    # step1: make a dict for both sites
-    zte_SiteDict = {}
-    sap_SiteDict = {}
-
-
-    # step2: delete all delete order and add site id to dn_record
-    count1 = len(sap_DNRecords)
-    ddns = [sdn for sdn in sap_DNRecords if sdn.Deletion_Indicator == 'L']
-    for ddn in ddns:
-        sap_DNRecords.remove(ddn)
-    count2 = len(sap_DNRecords)
-    sap_DNRecords = [sdn for sdn in sap_DNRecords if sdn.Still_to_be_delivered_qty != '0']
-    print("Delete sap_DNRecords processing: ", count1, count2, len(sap_DNRecords))
-
-    for sap_dn in sap_DNRecords:
-        if sap_dn.Order:
-            site_ids = [orbm.Equipment for orbm in orbm_Records if orbm.Order == sap_dn.Order]
-            site_ids = list(set(site_ids))
-            if len(site_ids) == 1:
-                sap_dn.Equipment = site_ids[0]
-            else:
-                sap_dn.Equipment = None
-                print("Error Step1: find more site_id for order", sap_dn.Order, site_ids)
-
-    # step3:
-    for sap_dn in sap_DNRecords:
-        if sap_dn.Equipment: # has site id
-            equipment = sap_dn.Equipment
-            material = sap_dn.Material
-            sappo = sap_dn.Purchasing_Document
-            if equipment not in sap_SiteDict.keys(): # this siteid not in dict
-                sap_SiteDict[equipment] = []
-            if equipment in sap_SiteDict.keys(): # after the last step, this site is already in
-                sap_SiteDict[equipment].append((sappo, material))
-
-
-
-
 
 
 
@@ -115,10 +77,82 @@ def mixFile(fileDict):
     bm_STRecors = fileDict["bm_STRecors"]
     orbm_Records = fileDict["orbm_Records"]
 
-
     result = []
-
     # step0: using sappo records to fill the ztepo record
+    zte_PORecords = addSPAPotoZtePo(sap_PORecords, zte_PORecords)
+    # step1:
+    # delete all sap_dnRedords with Delection_Indictor
+    # delete all order that has still to be delivery
+    sap_DNRecords  = deleteAlreadyDeliverydFromSPDnRecord(sap_DNRecords)
+    # step2: adding site id to SAP_DN
+    sap_DNRecords  = addSiteIDTOSPDnRecordFromOrderBmRecord(sap_DNRecords, orbm_Records)
+    # step3: add neid/BMID to sapdn_record
+    sap_DNRecords = addBMIDToSapDNFromOrderBmid(sap_DNRecords, orbm_Records)
+    # # step4: add bmid status to dn_record
+    sap_DNRecords = addBMIDStatusToSapDNRecordFromBMStatus(sap_DNRecords, bm_STRecors)
+    # step5: combain ztepo with dn_record
+    for sap_dn in sap_DNRecords:
+        if sap_dn.NotesID and sap_dn.Purchasing_Document and sap_dn.Material:
+            sap_dn.Unique_SPM = '-'.join([sap_dn.Equipment, sap_dn.Purchasing_Document, sap_dn.Material])
+            ztepos = [zpo for zpo in zte_PORecords if zpo.Unique_SPM == sap_dn.Unique_SPM]
+            ztepos = list(set(ztepos))
+            if len(ztepos) == 1:
+                print("OK Step5: Match SAPDN with ZTEPO")
+                zpo = ztepos[0]
+                for k, v in sap_dn.__dict__.items():
+                    zpo.__dict__[k] = v
+                result.append(zpo)
+
+    return result
+
+
+
+def deleteAlreadyDeliverydFromSPDnRecord(sap_DNRecords):
+    """
+    Delete all SAP DN Record, which was deleted or has nothing to delivery
+
+    :param sap_DNRecords: SPA DN Records
+    :return: the modified DN Records
+    """
+    count1 = len(sap_DNRecords)
+    ddns = [sdn for sdn in sap_DNRecords if sdn.Deletion_Indicator == 'L']
+    for ddn in ddns:
+        sap_DNRecords.remove(ddn)
+    count2 = len(sap_DNRecords)
+    sap_DNRecords = [sdn for sdn in sap_DNRecords if int(sdn.Still_to_be_delivered_qty) != 0]
+    print("Delete sap_DNRecords processing: ", count1, count2, len(sap_DNRecords))
+    return sap_DNRecords
+
+
+def addSiteIDTOSPDnRecordFromOrderBmRecord(sap_DNRecords, orbm_Records):
+    """
+    Adding SiteID (BSFE/ Equipment) to SPA_DN Record
+
+    :param sap_DNRecords:  the SAP DN record
+    :param orbm_Records: the order-bmid record
+    :return: the modified SAP DN Record
+    """
+    # step2: add site id to dn_record
+    for sap_dn in sap_DNRecords:
+        if sap_dn.Order:
+            site_ids = [orbm.Equipment for orbm in orbm_Records if orbm.Order == sap_dn.Order]
+            site_ids = list(set(site_ids))
+            if len(site_ids) == 1:
+                sap_dn.Equipment = site_ids[0]
+            else:
+                sap_dn.Equipment = None
+                print("Error Step1: find more site_id for order", sap_dn.Order, site_ids)
+    return sap_DNRecords
+
+
+def addSPAPotoZtePo(sap_PORecords, zte_PORecords):
+    """
+    Find all matched SAP PO to ZTE po
+
+    :param sap_PORecords: the SAP Po records
+    :param zte_PORecords: the ZTE Po records
+    :return: modified ZTE Po Records
+    """
     for zte_po in zte_PORecords:
         if zte_po.SAP_PO_Nr is None: # this record must find it's sap po
             sap_nrs = [spo.PurchNo for spo in sap_PORecords if spo.Reference_PO_Number == zte_po.ZTE_PO_Nr]
@@ -132,17 +166,16 @@ def mixFile(fileDict):
         else:
             zte_po.Unique_SPM = None
             zte_po.Unique_PM = None
+    return zte_PORecords
 
 
-    # step1:
-    # delete all sap_dnRedords with Delection_Indictor
-    # delete all order that has still to be delivery
+def addBMIDToSapDNFromOrderBmid(sap_DNRecords, orbm_Records):
+    """
 
-    sap_DNRecords  = deleteAlreadyDeliverydFromSPDnRecord(sap_DNRecords)
-    sap_DNRecords  = addSiteIDTOSPDnRecordFromOrderBmRecord(sap_DNRecords, orbm_Records)
-
-
-    # step3: add neid to dn_record
+    :param sap_DNRecords:
+    :param orbm_Records:
+    :return:
+    """
     for sap_dn in sap_DNRecords:
         if sap_dn.Equipment and sap_dn.Order: # if this dn has site_id
             NotesIDs = [orbm.NotesID for orbm in orbm_Records if orbm.Order == sap_dn.Order
@@ -154,7 +187,8 @@ def mixFile(fileDict):
                 sap_dn.NotesID = None
                 print("Error Step3: find zero or more NotesID for order", sap_dn.Order, sap_dn.Equipment, NotesIDs)
 
-    # # step4: add bmid status to dn_record
+
+def addBMIDStatusToSapDNRecordFromBMStatus(sap_DNRecords, bm_STRecors):
     for sap_dn in sap_DNRecords:
         if sap_dn.NotesID and sap_dn.Equipment: # if has bmid
             bms = [bm for bm in bm_STRecors if bm.BAUMASSNAHME_ID == sap_dn.NotesID
@@ -175,48 +209,6 @@ def mixFile(fileDict):
                 sap_dn.PRICING = None
                 sap_dn.GEMEINDE_NAME = None
                 print("Error Step4:: find zero or more BMID Recorf for NotesID", sap_dn.NotesID,'Equipment', sap_dn.Equipment, len(bms))
-
-    #
-    # step5: combain ztepo with dn_record
-    for sap_dn in sap_DNRecords:
-        if sap_dn.NotesID and sap_dn.Purchasing_Document and sap_dn.Material:
-            sap_dn.Unique_SPM = '-'.join([sap_dn.Equipment, sap_dn.Purchasing_Document, sap_dn.Material])
-            ztepos = [zpo for zpo in zte_PORecords if zpo.Unique_SPM == sap_dn.Unique_SPM]
-            ztepos = list(set(ztepos))
-            if len(ztepos) == 1:
-                print("OK Step5: Match SAPDN with ZTEPO")
-                zpo = ztepos[0]
-                for k, v in sap_dn.__dict__.items():
-                    zpo.__dict__[k] = v
-                result.append(zpo)
-
-    return result
-
-
-
-def deleteAlreadyDeliverydFromSPDnRecord(sap_DNRecords):
-    count1 = len(sap_DNRecords)
-    ddns = [sdn for sdn in sap_DNRecords if sdn.Deletion_Indicator == 'L']
-    for ddn in ddns:
-        sap_DNRecords.remove(ddn)
-    count2 = len(sap_DNRecords)
-    sap_DNRecords = [sdn for sdn in sap_DNRecords if int(sdn.Still_to_be_delivered_qty) != 0]
-    print("Delete sap_DNRecords processing: ", count1, count2, len(sap_DNRecords))
-    return sap_DNRecords
-
-
-def addSiteIDTOSPDnRecordFromOrderBmRecord(sap_DNRecords, orbm_Records):
-    # step2: add site id to dn_record
-    for sap_dn in sap_DNRecords:
-        if sap_dn.Order:
-            site_ids = [orbm.Equipment for orbm in orbm_Records if orbm.Order == sap_dn.Order]
-            site_ids = list(set(site_ids))
-            if len(site_ids) == 1:
-                sap_dn.Equipment = site_ids[0]
-            else:
-                sap_dn.Equipment = None
-                print("Error Step1: find more site_id for order", sap_dn.Order, site_ids)
-    return sap_DNRecords
 
 
 
