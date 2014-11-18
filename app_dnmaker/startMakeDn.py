13b0e656af7f55f984421a9b6f1bae8211dd2099
yuankunluo <yuankun1988@gmail.com>
2014-11-18 13:35:07 +0100

    fix bugs

--- app_dnmaker/startMakeDn.py
+++ app_dnmaker/startMakeDn.py
@@ -20,9 +20,31 @@ def getAllData():
     raw_dict['sappos'] = sappos
     raw_dict['sappns'] = sappns
     raw_dict['orbmids'] = orbmids
-    
-    recordReader.storeRawData()
-    return (sappos, sappns, orbmids, ztepos, bmstatus)
+    raw_dict['ztepos'] = ztepos
+    raw_dict['bmstatus'] = bmstatus
+    recordReader.storeRawData(raw_dict,"Raw_data_dict",'output/raw/')
+    return raw_dict
+
+
+def doProductDN(raw_dict):
+    """
+
+    :param raw_dict:
+    :return:
+    """
+    sappos = raw_dict['sappos']
+    sappns =raw_dict['sappns']
+    orbmids = raw_dict['orbmids']
+    ztepos = raw_dict['ztepos']
+    bmstatus = raw_dict['bmstatus']
+
+    sappos = step1_MixSapPOandZTEPO(sappos, ztepos)
+    sappos = step2_AddingSAPDNtoSappo(sappos, sappns)
+    sappos = step3_AddingBMIDandBsfeToSAPPO(sappos, orbmids)
+    sappos = step4_AddBmstatusToSappos(sappos, bmstatus)
+
+    return sappos
+
 
 def step1_MixSapPOandZTEPO(sappos = None, ztepos= None):
     """
@@ -37,99 +59,172 @@ def step1_MixSapPOandZTEPO(sappos = None, ztepos= None):
     if ztepos == None:
         ztepos = recordReader.loadRawData('output/raw/Raw_0_ZTEPOLIST.raw')
 
-
-    print("ZPO has not CM:", len(ztepos))
-    # 1 make two dict, one for sappo without ref, one for sappo with ref
-    sappo_without_ref = {}
-    sappo_with_ref = {}
-    # add sappo to these dict
-    saprefCount = 0
-    sapnorefCount = 0
+    # ------------------- new
+    # build dicts
+    refpo_dict = {}
+    sappo_dict = {}
+    ref_count = 0
+    noref_count = 0
+    ref_dupCount = 0
+    noref_dupcount = 0
     for sappo in sappos:
-        # check if this sappo has ref
-        if sappo.Reference_PO_Number:
-            # add it to refdict
-            if sappo.Reference_PO_Number not in sappo_with_ref:
-                sappo_with_ref[sappo.Reference_PO_Number] = set()
-            sappo_with_ref[sappo.Reference_PO_Number].add(sappo)
-            saprefCount += 1
-        else:
-            # add it to noref dict
-            if sappo.PurchNo not in sappo_without_ref:
-                sappo_without_ref[sappo.PurchNo] = set()
-            sappo_without_ref[sappo.PurchNo].add(sappo)
-            sapnorefCount += 1
-    print("Step1: Refcout", saprefCount, "No ref: ", sapnorefCount,
-          "Total record ", len(sappos), "Diff: ", len(sappos) - (sapnorefCount + saprefCount))
-
-    # 2 combine ztepo with sappo
-    normalMatch = []
-    normalDisMatch = []
-    refMatch = []
-    refDisMatch = []
-    nosapMatch = []
-    for zpo in ztepos:
-        # if this zpo has ztepor
-        if zpo.ZTE_PO_Nr:
-            # first check if this zponr is a refnr
-            if zpo.ZTE_PO_Nr in sappo_with_ref:
-                sappolist = list(sappo_with_ref[zpo.ZTE_PO_Nr])
-                for sappo in sappolist:
-                    # check math
-                    if (sappo.Reference_PO_Number == zpo.ZTE_PO_Nr
-                        and sappo.Material == zpo.ZTE_Material
-                        # and sappo.PO_Quantity == zpo.ZTE_Qty
-                    ):
-                        newZPO = copy.deepcopy(zpo)
-                        for k, v in sappo.__dict__.items():
-                            newZPO.__dict__[k] = v
-                        refMatch.append(newZPO)
-                    else:
-                        refDisMatch.append(zpo)
-                continue
-            # check if it is a normal sappo
-            if zpo.ZTE_PO_Nr in sappo_without_ref:
-                # do compare if this zpo record is right
-                sappolist = list(sappo_without_ref[zpo.ZTE_PO_Nr])
-                # compare item for item
-                for sappo in sappolist:
-                    # if every thing match
-                    if (zpo.ZTE_PO_Nr == sappo.PurchNo
-                        and zpo.ZTE_Material == sappo.Material
-                        # and zpo.ZTE_Qty == sappo.PO_Quantity
-                    ):
-                        newZPO = copy.deepcopy(zpo)
-                        # copy the information in sappo into zpo
-                        for k, v in sappo.__dict__.items():
-                            newZPO.__dict__[k] = v
-                        normalMatch.append(newZPO)
-                    else:
-                        normalDisMatch.append(zpo)
-                continue
+        if sappo.Reference_PO_Number and sappo.Material:
+            key_PMQ = (sappo.Reference_PO_Number, sappo.Material)
+            if key_PMQ not in refpo_dict:
+                refpo_dict[key_PMQ] = set()
             else:
-                nosapMatch.append(zpo)
+                ref_dupCount += 1
+            refpo_dict[key_PMQ].add(sappo)
+            ref_count += 1
+            continue
+        if sappo.Material and sappo.PurchNo:
+            key_PMQ = (sappo.PurchNo, sappo.Material)
+            if key_PMQ not in sappo_dict:
+                sappo_dict[key_PMQ] = set()
+            else:
+                noref_dupcount += 1
+            sappo_dict[key_PMQ].add(sappo)
+            noref_count += 1
+            continue
+
+    print("Ref po ",ref_count, "Noref po ",noref_count,
+          "Dup Count", ref_dupCount, noref_dupcount,
+          "Total sappos ", len(sappos),
+          "Diff", len(sappos) - ref_count - noref_count)
 
 
-    print("Step1: Match rate:",
-          "Normalmatch", len(normalMatch), "Normaldismatch",len(normalDisMatch),
-          "Refmatch", len(refMatch), "Refdismatch", len(refDisMatch),
-    )
 
-    normalMatch.extend(refMatch)
-    normalDisMatch.extend(refDisMatch)
-    print("Step1: Total match rate", len(normalMatch), len(sappos),
-          "Total dismatch rate", len(normalDisMatch), len(sappos)
-    )
+    result = []
+    nomatchzpo = []
+    for zpo in ztepos:
+        if zpo.ZTE_PO_Nr and zpo.ZTE_Material:
+            zpo_unique_PM = (zpo.ZTE_PO_Nr, zpo.ZTE_Material)
+            # check if this zpo in sappo
+            if zpo_unique_PM in sappo_dict:
+                sappo_set = sappo_dict[zpo_unique_PM]
+                sappo_list = list(sappo_set)
+                for sappo in sappo_list:
+                    for k, v in zpo.__dict__.items():
+                        sappo.__dict__[k] = v
+                    result.append(sappo)
+            elif zpo_unique_PM in refpo_dict:
+                sappo_set = refpo_dict[zpo_unique_PM]
+                sappo_list = list(sappo_set)
+                for sappo in sappo_list:
+                    for k, v in zpo.__dict__.items():
+                        sappo.__dict__[k] = v
+                    result.append(sappo)
+            else:
+                nomatchzpo.append(zpo)
 
-    fileWriter.outputObjectsToFile(normalMatch, 'Step1_ZPO_SAP_Match','output/dn_maker/')
-    fileWriter.outputObjectsToFile(normalDisMatch, 'Step1_ZPO_SAP_Dismatch','output/error/')
-    fileWriter.outputObjectsToFile(nosapMatch, 'Step1_ZPO_without_SAPPO','output/error/')
-    recordReader.storeRawData(normalMatch, 'Step1_ZPO_SAP_Match','output/raw/')
-    normalMatch.extend(normalDisMatch)
-    fileWriter.outputObjectsToFile(normalMatch, 'Step1_mixed_ZPO','output/dn_maker/')
+    print(len(result), len(sappos), len(nomatchzpo))
 
 
-    return normalMatch
+    fileWriter.outputObjectsToFile(result, 'Step1_ZPO_SAP_Match','output/dn_maker/')
+    fileWriter.outputObjectsToFile(nomatchzpo, 'Step1_ZPO_SAP_Dismatch','output/error/')
+    recordReader.storeRawData(result, 'Step1_ZPO_SAP_Match','output/raw/')
+    fileWriter.outputObjectsToFile(sappos, 'Step1_mixed_ZPO','output/dn_maker/')
+
+
+
+
+
+
+
+    # # ------------------- old
+    # print("ZPO has not CM:", len(ztepos))
+    # # 1 make two dict, one for sappo without ref, one for sappo with ref
+    # sappo_without_ref = {}
+    # sappo_with_ref = {}
+    # # add sappo to these dict
+    # saprefCount = 0
+    # sapnorefCount = 0
+    # for sappo in sappos:
+    #     # check if this sappo has ref
+    #     if sappo.Reference_PO_Number:
+    #         # add it to refdict
+    #         if sappo.Reference_PO_Number not in sappo_with_ref:
+    #             sappo_with_ref[sappo.Reference_PO_Number] = set()
+    #         sappo_with_ref[sappo.Reference_PO_Number].add(sappo)
+    #         saprefCount += 1
+    #     else:
+    #         # add it to noref dict
+    #         if sappo.PurchNo not in sappo_without_ref:
+    #             sappo_without_ref[sappo.PurchNo] = set()
+    #         sappo_without_ref[sappo.PurchNo].add(sappo)
+    #         sapnorefCount += 1
+    # print("Step1: Refcout", saprefCount, "No ref: ", sapnorefCount,
+    #       "Total record ", len(sappos), "Diff: ", len(sappos) - (sapnorefCount + saprefCount))
+    #
+    # # 2 combine ztepo with sappo
+    # normalMatch = []
+    # normalDisMatch = []
+    # refMatch = []
+    # refDisMatch = []
+    # nosapMatch = []
+    # for zpo in ztepos:
+    #     # if this zpo has ztepor
+    #     if zpo.ZTE_PO_Nr:
+    #         # first check if this zponr is a refnr
+    #         if zpo.ZTE_PO_Nr in sappo_with_ref:
+    #             sappolist = list(sappo_with_ref[zpo.ZTE_PO_Nr])
+    #             for sappo in sappolist:
+    #                 # check math
+    #                 if (sappo.Reference_PO_Number == zpo.ZTE_PO_Nr
+    #                     and sappo.Material == zpo.ZTE_Material
+    #                     # and sappo.PO_Quantity == zpo.ZTE_Qty
+    #                 ):
+    #                     newZPO = copy.deepcopy(zpo)
+    #                     for k, v in sappo.__dict__.items():
+    #                         newZPO.__dict__[k] = v
+    #                     refMatch.append(newZPO)
+    #                 else:
+    #                     refDisMatch.append(zpo)
+    #             continue
+    #         # check if it is a normal sappo
+    #         if zpo.ZTE_PO_Nr in sappo_without_ref:
+    #             # do compare if this zpo record is right
+    #             sappolist = list(sappo_without_ref[zpo.ZTE_PO_Nr])
+    #             # compare item for item
+    #             for sappo in sappolist:
+    #                 # if every thing match
+    #                 if (zpo.ZTE_PO_Nr == sappo.PurchNo
+    #                     and zpo.ZTE_Material == sappo.Material
+    #                     # and zpo.ZTE_Qty == sappo.PO_Quantity
+    #                 ):
+    #                     newZPO = copy.deepcopy(zpo)
+    #                     # copy the information in sappo into zpo
+    #                     for k, v in sappo.__dict__.items():
+    #                         newZPO.__dict__[k] = v
+    #                     normalMatch.append(newZPO)
+    #                 else:
+    #                     normalDisMatch.append(zpo)
+    #             continue
+    #         else:
+    #             nosapMatch.append(zpo)
+    #
+    #
+    # print("Step1: Match rate:",
+    #       "Normalmatch", len(normalMatch), "Normaldismatch",len(normalDisMatch),
+    #       "Refmatch", len(refMatch), "Refdismatch", len(refDisMatch),
+    # )
+    #
+    # normalMatch.extend(refMatch)
+    # normalDisMatch.extend(refDisMatch)
+    # print("Step1: Total match rate", len(normalMatch), len(sappos),
+    #       "Total dismatch rate", len(normalDisMatch), len(sappos)
+    # )
+    #
+    # mixztpos = copy.deepcopy(normalMatch)
+    # mixztpos.extend(normalDisMatch)
+    #
+    # fileWriter.outputObjectsToFile(normalMatch, 'Step1_ZPO_SAP_Match','output/dn_maker/')
+    # fileWriter.outputObjectsToFile(normalDisMatch, 'Step1_ZPO_SAP_Dismatch','output/error/')
+    # fileWriter.outputObjectsToFile(nosapMatch, 'Step1_ZPO_without_SAPPO','output/error/')
+    # recordReader.storeRawData(normalMatch, 'Step1_ZPO_SAP_Match','output/raw/')
+    # fileWriter.outputObjectsToFile(mixztpos, 'Step1_mixed_ZPO','output/dn_maker/')
+    #
+    # return normalMatch
 
 def step2_AddingSAPDNtoSappo(sappos = None, sapdns = None):
     """
@@ -317,7 +412,9 @@ def step4_AddBmstatusToSappos(sappos, bmstatus):
 
     dns = [sappo for sappo in result if sappo.IST92
            and not sappo.ZTE_CM_No
-           and int(sappo.Still_to_be_delivered_qty) != 0]
+           and int(sappo.Still_to_be_delivered_qty) != 0
+           and not sappo.Deletion_Indicator
+    ]
 
     fileWriter.outputObjectsToFile(result, 'Step4_SAPPO_with_BMSTATUS','output/dn_maker/')
     fileWriter.outputObjectsToFile(nomatch, 'Step3_SAPPO_without_BMSTATUS','output/error/')
