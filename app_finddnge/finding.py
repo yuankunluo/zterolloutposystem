4ada21da6560fd7a0f609b020bc8a6ba04481faf
yuankunluo <yuankun1988@gmail.com>
2014-12-22 14:36:32 +0100

    find dnge over

---
+++ app_finddnge/finding.py
@@ -0,0 +1,45 @@
+__author__ = 'yuankunluo'
+
+
+import tools.fileReader as fileReader
+import tools.fileWriter as fileWriter
+import os
+import re
+
+
+
+def goToFindDNGEFromListInDirectory(findlist='input/finddnge_list/', dngesinput='input/dnge/',
+                                outputpath='output/finddnge/'):
+    """
+
+    :param findlist:
+    :param dngesinput:
+    :param output:
+    :return:
+    """
+
+    row_list = fileReader.getAllRowObjectInPath(findlist,True)
+    dnge_list = [r.DNGE for r in row_list]
+    file_list = []
+    # print(dnge_list)
+
+
+    find_count = 0
+    for (dirpath, dirnames, filenames) in os.walk(dngesinput):
+        for fname in filenames:
+            fname_clean = re.sub('\.xls?x' , '', fname, re.IGNORECASE)
+            fname_clean = unicode(fname_clean)
+            file_list.append(fname_clean)
+            if fname_clean in dnge_list:
+                content = None
+                with open(dirpath + "/" + fname, "rb") as f:
+                    content = f.read()
+                with open(outputpath + fname, "wb") as f2:
+                    f2.write(content)
+                find_count += 1
+
+    print("Find %d in %d" % (find_count, len(dnge_list)))
+    print("Done")
+
+    return dnge_list, file_list
+
