2c0087788ec77ac5fe657d5e44ef7257bc45c6e3
yuankunluo <yuankun1988@gmail.com>
2014-12-22 15:47:56 +0100

    change regx to match file

--- app_finddnge/finding.py
+++ app_finddnge/finding.py
@@ -27,16 +27,21 @@ def goToFindDNGEFromListInDirectory(findlist='input/finddnge_list/', dngesinput=
     find_count = 0
     for (dirpath, dirnames, filenames) in os.walk(dngesinput):
         for fname in filenames:
-            fname_clean = re.sub('(\.xls?x)|(\.pdf)', '', fname, re.IGNORECASE)
-            fname_clean = unicode(fname_clean)
-            file_list.append(fname_clean)
-            if fname_clean in dnge_list:
-                content = None
-                with open(dirpath + "/" + fname, "rb") as f:
-                    content = f.read()
-                with open(outputpath + fname, "wb") as f2:
-                    f2.write(content)
-                find_count += 1
+
+            #fname_clean = re.sub('(\.xls?x)|(\.pdf)', '', fname, re.IGNORECASE)
+            reg = '.*(DNGE\d+).*'
+            match = re.match(reg, fname)
+            if match:
+                fname_clean = match.groups()[0]
+                fname_clean = unicode(fname_clean)
+                file_list.append(fname_clean)
+                if fname_clean in dnge_list:
+                    content = None
+                    with open(dirpath + "/" + fname, "rb") as f:
+                        content = f.read()
+                    with open(outputpath + fname, "wb") as f2:
+                        f2.write(content)
+                    find_count += 1
 
     print("Find %d in %d" % (find_count, len(dnge_list)))
     print("Done")
